7cb2780a921cc5afb9b08fba394867fea0152994
Nuclominus <1941302+Nuclominus@users.noreply.github.com>
2025-08-11 01:34:41 +0300

    update app version to 1.0.3 in configuration settings

--- build-logic/convention/src/main/kotlin/data/Config.kt
+++ build-logic/convention/src/main/kotlin/data/Config.kt
@@ -3,7 +3,7 @@ package data
 import org.gradle.api.JavaVersion
 
 object AndroidAppConf {
-    const val APP_VERSION: String = "1.0.2"
+    const val APP_VERSION: String = "1.0.3"
     const val VERSION_CODE: Int = 7
     const val NAMESPACE: String = "io.github.nuclominus.imforge.app"
     const val COMPILE_SDK: Int = 36
